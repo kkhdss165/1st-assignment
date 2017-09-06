0470dfc64d610a07211b0190f0388f18cef863c0
kkhdss165 <31638890+kkhdss165@users.noreply.github.com>
2017-09-06 12:03:36 +0900

    Add files via upload
    1st

---
+++ Assignment_1st.java
@@ -0,0 +1,13 @@
+//2016301007	김경호
+public class Assignment_1st 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("학번 : 2016301007");
+		System.out.println("이름 : 김경호");
+		System.out.println("전화번호 : 010 4749 0342");
+		System.out.println("GitHub address : https://github.com/kkhdss165");
+	}
+
+}
